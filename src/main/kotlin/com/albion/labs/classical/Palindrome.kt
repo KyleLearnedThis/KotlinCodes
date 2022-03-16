71628bd76b108a21126f00ccf421b22053861591
KyleLearnedThis <kyle.learned.this@gmail.com>
2022-03-15 17:35:51 -0700

    Another way to solve Palindrome with test

--- src/main/kotlin/com/albion/labs/classical/Palindrome.kt
+++ src/main/kotlin/com/albion/labs/classical/Palindrome.kt
@@ -11,5 +11,20 @@ class Palindrome {
             val reverse = input.reversed()
             return reverse == input
         }
+
+        fun validPalindromeV2(s: String): Boolean {
+            val arr = s.filter{it.isLetterOrDigit()}
+                .lowercase()
+                .toCharArray()
+            val size = arr.size
+            for(i in 0 .. arr.size / 2) {
+                val l = arr[i]
+                val r = arr[size - i - 1]
+                if(l != r) {
+                    return false
+                }
+            }
+            return true
+        }
     }
 }
